ce48877986ffaac351f46bcdc3fb06f91f99b8b4
Kayo Fellipe <167824869+Kayo-Fellipe@users.noreply.github.com>
2025-09-15 15:53:44 -0300

    UP

--- src/main.tsx
+++ src/main.tsx
@@ -12,3 +12,19 @@ createRoot(rootElement).render(
     <App />
   </React.StrictMode>
 );
+
+
+
+
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
